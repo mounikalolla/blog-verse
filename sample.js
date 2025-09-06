7b197ea439528938663557d3d8d903dd11a33bd2
mounikalolla <mounikalolla335@gmail.com>
2025-09-06 12:36:23 +0530

    system

--- sample.js
+++ sample.js
@@ -19,9 +19,9 @@ let arr=[1,2,3,4,5]
 console.log(arr[2])
 console.log(typeof arr)
 let person ={
-    name="mouni"
-    age=18
+    name:"mouni",
+    age:8
 }
 console.log(person.name)
 console.log(person.age)
-console.lod(typeof person)
+console.log(typeof person)
