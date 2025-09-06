1ab8af27c1ec682b969ac49c9e1a2f57ab6ca877
mounikalolla <mounikalolla335@gmail.com>
2025-09-06 12:33:44 +0530

    system

--- sample.js
+++ sample.js
@@ -1,1 +1,27 @@
 console.log("this is a sample file")
+
+let name="mounika"
+console.log(name)
+let num=18
+console.log(num)
+console.log(typeof name)
+console.log(typeof num)
+let islogged=false
+console.log(islogged)
+console.log(typeof islogged)
+let x
+console.log(x)
+console.log(typeof x)
+let y=null
+console.log(y)
+console.log(typeof y)
+let arr=[1,2,3,4,5]
+console.log(arr[2])
+console.log(typeof arr)
+let person ={
+    name="mouni"
+    age=18
+}
+console.log(person.name)
+console.log(person.age)
+console.lod(typeof person)
