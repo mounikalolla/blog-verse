699f83101c7f38a68b8cddcc9660b2f9a62788a4
mounikalolla <mounikalolla335@gmail.com>
2025-09-06 15:54:00 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,29 @@
+function greet()
+{
+    console.log("welcome to devastra")
+}
+greet()
+
+function add(x,y)
+{
+    return x+y
+}
+let result=add(20,28)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+const multiply=function(x,y)
+{
+    return x*y
+}
+console.log(multiply(5,3))
+
+//arrow function 
+const square=(x)=>x*x;
+console.log(square(5))
+const subtract=(x,y)=>{
+    return x-y;
+}
+console.log(subtract(20,15))
